7a0a153d2cdcfecb8bb2ebdcd1f263ce3b21fd99
Agusti <6601142+agustif@users.noreply.github.com>
2023-06-15 19:19:36 +0200

    fix: traducir tooltip borrar al español

--- src/components/DeleteConfirmationModal.tsx
+++ src/components/DeleteConfirmationModal.tsx
@@ -9,9 +9,9 @@ const DeleteConfirmationModal: React.FC<DeleteConfirmationModalProps> = ({
 }) => {
   return (
     <div className="tooltip">
-      <p>Are you sure you want to delete this property?</p>
-      <button className="confirm" onClick={onConfirm}>Confirm</button>
-      <button className="cancel" onClick={onCancel}>Cancel</button>
+      <p>¿Estas seguro de borrar esta propiedad?</p>
+      <button className="confirm" onClick={onConfirm}>Confirmar</button>
+      <button className="cancel" onClick={onCancel}>Cancelar</button>
     </div>
   );
 };
