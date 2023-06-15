0979557e1362e2b6a9ee96908de8e46969ba0e77
Agusti <6601142+agustif@users.noreply.github.com>
2023-06-15 17:04:33 +0200

    feat: PDF MVP generation works

--- src/utils/pdfUtils.ts
+++ src/utils/pdfUtils.ts
@@ -1,45 +1,87 @@
 import pdfMake from 'pdfmake/build/pdfmake';
 import pdfFonts from 'pdfmake/build/vfs_fonts';
 import Property from '../types/Property';
+import { Margins, TDocumentDefinitions, ContentColumns, ContentUnorderedList } from 'pdfmake/interfaces';
 
 pdfMake.vfs = pdfFonts.pdfMake.vfs;
 
 const downloadPDF = (properties: Property[]) => {
   // Define the document definition for PDFMake
-  const documentDefinition = {
+  const documentDefinition: TDocumentDefinitions = {
     content: [
-      { text: 'Property List', style: 'header' },
-      { text: 'Sorted by price', style: 'subheader' },
+      {
+        image: 'logo',
+        width: 200,
+        height: 100,
+        absolutePosition: { x: 380, y: 20 },
+      },
+      { text: 'Properties List', style: 'header' },
+      { text: 'by price', style: 'subheader' },
       { text: '\n' }, // Add line break
 
-      // Generate a table for properties
-      {
-        table: {
-          headerRows: 1,
-          widths: ['auto', 'auto', 'auto', 'auto'],
-          body: [
-            ['Title', 'Rooms', 'Bathrooms', 'Price'],
-            ...properties.map((property: Property) => [
-              property.title,
-              property.rooms.toString(),
-              property.bathrooms.toString(),
-              property.price.toString(),
-            ]),
+      // Generate a list of properties
+      ...properties.flatMap((property: Property, index) => [
+        {
+          columns: [
+            {
+              image: 'thumbnail',
+              width: 180,
+              height: 100,
+              alignment: 'left', // Align thumbnail image to the left
+            },
+            {
+              width: '*',
+              stack: [
+                { text: `${index + 1} - ${property.title}`, style: 'propertyTitle' },
+                '\n',
+                `${property.rooms} habitaciones`,
+                `${property.bathrooms} baño`,
+                {
+                  text: `${property.price}€/mes`,
+                  style: 'propertyPrice',
+                },
+              ],
+            },
           ],
+          columnGap: 10, // Add gap between thumbnail and property details
+          marginBottom: 12, // Add margin at the bottom of each property
         },
-      },
-    ],
+        { canvas: [{ type: 'line', x1: 0, y1: 5, x2: 495, y2: 5 }] }, // Add line break separator
+        // add blank space
+        { text: '\n' },
+      ]),
+    ] as unknown as (ContentColumns | ContentUnorderedList | { canvas: any })[],
+    images: {
+      thumbnail: 'https://source.unsplash.com/random/180x100?house,empty',
+      // FIXME: replace localhost for ENV var for vercel deployment.
+      logo: 'http://localhost:3000/logo.png',
+    },
     styles: {
       header: {
-        fontSize: 18,
+        fontSize: 24,
         bold: true,
+        marginTop: 128,
         marginBottom: 10,
       },
       subheader: {
         fontSize: 14,
-        bold: true,
-        marginBottom: 5,
+        bold: false,
+        marginTop: -28,
+        marginLeft: 168,
+        marginBottom: 20,
       },
+      propertyTitle: {
+        fontSize: 18,
+        color: 'black',
+        background: '#FFA500',
+        bold: true,
+      },
+      propertyPrice: {
+        fontSize: 14,
+        bold: true,
+        margin: [0, 5, 0, 10],
+      },
+      thumbnailImage: {},
     },
   };
 
@@ -49,8 +91,8 @@ const downloadPDF = (properties: Property[]) => {
   const blob = pdfDocGenerator.getBlob((blob) => {
     console.log(blob);
     return blob;
-}); 
-return blob;
+  });
+  return blob;
 };
 
 export { downloadPDF };
